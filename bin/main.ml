4089da3d981f3893461f070ccce44abc49dfcb93
James Tan <james_tan97@outlook.com>
2025-10-03 23:21:13 -0400

    change position in main.ml

--- bin/main.ml
+++ bin/main.ml
@@ -4,15 +4,12 @@ open Types
 module P = Position.Position
 module S = Search
 
-(* EASY: Scholar's mate *)
 (* TODO: make UCI server *)
 let _ =
-  let pos =
-    P.from_fen "r1bqkb1r/pppp1ppp/2n2n2/4p2Q/2B1P3/8/PPPP1PPP/RNB1K1NR w KQkq - 4 4"
-  in
+  let pos = P.from_fen "8/2b2r1P/2P4k/1pK3n1/1N1R1N2/nqp5/8/8 b - - 0 1" in
   match pos with
   | Error _ -> assert false
   | Ok pos ->
-    let best_move = S.get_best_move pos 2 0 in
+    let best_move = S.get_best_move pos 5 0 in
     Stdlib.Printf.printf "best move is %s\n" (Types.show_move best_move)
 ;;
