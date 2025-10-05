cd1af43aa12a8fb6e08345dccade6515041c2f22
James Tan <james_tan97@outlook.com>
2025-10-04 22:11:35 -0400

    only generate moves after trying hash move

--- lib/search.ml
+++ lib/search.ml
@@ -14,6 +14,7 @@ let rec alpha_beta pos curr_depth max_depth alpha beta is_white ply history ~may
   let remaining_depth = max_depth - curr_depth in
   let offset = if is_white then -1 else 1 in
   let eval_value = offset * Eval.evaluate pos () in
+  let is_in_check = P.is_in_check pos in
   let do_move (alpha, best_move) move =
     let score =
       -1
@@ -46,7 +47,14 @@ let rec alpha_beta pos curr_depth max_depth alpha beta is_white ply history ~may
     then Continue_or_stop.Continue (score, Some move)
     else Continue_or_stop.Continue (alpha, best_move)
   in
-  let process_tt_entry depth (tt_entry : TT.entry option) =
+  let do_move' alpha move =
+    match do_move (alpha, None) move with
+    | Continue_or_stop.Continue (score, m) -> score, m, false
+    | Continue_or_stop.Stop (score, m, cut) -> score, m, cut
+  in
+  (* Try to get an early exit score from the TT entry, also evaluates the hash
+    move if it exists *)
+  let process_tt_entry depth (tt_entry : TT.entry option) alpha =
     match tt_entry with
     | Some tt_entry ->
       let score =
@@ -58,82 +66,91 @@ let rec alpha_beta pos curr_depth max_depth alpha beta is_white ply history ~may
         | true, TT.BOUND_UPPER when tt_entry.value < alpha -> Some tt_entry.value
         | _, _ -> None
       in
-      let move =
-        match tt_entry.bound with
-        | (TT.BOUND_EXACT | TT.BOUND_LOWER)
-          when T.move_not_none tt_entry.move && P.legal pos tt_entry.move ->
-          Some tt_entry.move
-        | _ -> None
-      in
-      score, move
-    | None -> None, None
+      (match score, tt_entry.bound with
+       | Some _, _ -> score, alpha
+       | _, (TT.BOUND_EXACT | TT.BOUND_LOWER)
+         when T.move_not_none tt_entry.move && P.legal pos tt_entry.move ->
+         let score, _, is_cut = do_move' alpha tt_entry.move in
+         if is_cut then Some score, alpha else None, score
+       | _, _ -> score, alpha)
+    | None -> None, alpha
   in
-  let legal_moves = generate_moves pos in
   (* This takes into account the 50 move rule and threehold repetition *)
   if P.is_draw pos ply
   then T.value_draw
-  else if List.is_empty legal_moves
-  then
-    (* Either draw or mate *)
-    if P.is_in_check pos then -(T.value_mate - curr_depth) else T.value_draw
-  else if remaining_depth = 0 || curr_depth = T.max_ply
+  else if (not is_in_check) && (remaining_depth <= 0 || curr_depth = T.max_ply)
   then
     (* TODO: quiescence search *)
     eval_value
-  else if remaining_depth = 1 && may_prune && eval_value + T.futility_margin_1 < alpha
+  else if
+    (not is_in_check)
+    && remaining_depth = 1
+    && may_prune
+    && eval_value + T.futility_margin_1 < alpha
   then
     (* If a move proves to be futile, we just return alpha since *)
     (* further continuations are unlikely to raise alpha *)
     alpha
-  else if remaining_depth = 2 && may_prune && eval_value + T.futility_margin_2 < alpha
+  else if
+    (not is_in_check)
+    && remaining_depth = 2
+    && may_prune
+    && eval_value + T.futility_margin_2 < alpha
   then
     (* Same as above *)
     alpha
   else (
     let tt_entry = TT.probe tt pos.st.key in
-    match process_tt_entry remaining_depth tt_entry with
+    match process_tt_entry remaining_depth tt_entry alpha with
     | Some score, _ -> score
-    | _, hash_move ->
-      (* Move ordering *)
-      (* 1. Hash move *)
-      (* 2. Good captures *)
-      (* 3. Bad captures *)
-      (* 4. Non-captures *)
-      let sorted_moves =
-        List.map legal_moves ~f:(fun m ->
-          let score =
-            if Option.equal T.equal_move hash_move (Some m)
-            then Int.max_value
-            else if P.is_capture pos m
-            then
-              if P.see_ge pos m 1
-              then
-                (* TODO: maybe some captures are better than others? *)
-                10
-              else 0
-            else Int.min_value
-          in
-          m, score)
-        |> List.sort ~compare:(fun (_, v1) (_, v2) -> compare v2 v1)
-        |> List.map ~f:fst
-      in
-      let score, best_move, is_cut =
-        (* no cutoff if we finish *)
-        List.fold_until sorted_moves ~init:(alpha, None) ~f:do_move ~finish:(fun (a, b) ->
-          a, b, false)
-      in
-      if not is_cut
+    | _, alpha ->
+      let legal_moves = M.generate_legal pos in
+      if List.is_empty legal_moves
       then
-        ignore
-        @@ TT.store
-             tt
-             ~key:pos.st.key
-             ~m:(Stdlib.Option.value best_move ~default:T.none_move)
-             ~depth:remaining_depth
-             ~eval_value
-             ~value:score
-             ~bound:(if Option.is_some best_move then TT.BOUND_EXACT else TT.BOUND_UPPER);
-      score)
+        (* Either draw or mate *)
+        if P.is_in_check pos then -(T.value_mate - curr_depth) else T.value_draw
+      else (
+        (* Move ordering *)
+        (* 1. Good captures *)
+        (* 2. Bad captures *)
+        (* 3. Non-captures *)
+        let sorted_moves =
+          List.map legal_moves ~f:(fun m ->
+            let score =
+              if P.is_capture pos m
+              then
+                if P.see_ge pos m 1
+                then
+                  (* TODO: maybe some captures are better than others? *)
+                  10
+                else 0
+              else Int.min_value
+            in
+            m, score)
+          |> List.sort ~compare:(fun (_, v1) (_, v2) -> compare v2 v1)
+          |> List.map ~f:fst
+        in
+        let score, best_move, is_cut =
+          (* no cutoff if we finish *)
+          List.fold_until
+            sorted_moves
+            ~init:(alpha, None)
+            ~f:do_move
+            ~finish:(fun (a, b) -> a, b, false)
+        in
+        if not is_cut
+        then
+          ignore
+          @@ TT.store
+               tt
+               ~key:pos.st.key
+               ~m:(Stdlib.Option.value best_move ~default:T.none_move)
+               ~depth:remaining_depth
+               ~eval_value
+               ~value:score
+               ~bound:
+                 (if Option.is_some best_move then TT.BOUND_EXACT else TT.BOUND_UPPER);
+        score))
 ;;
 
 let get_best_move (pos : P.t) max_depth ply : T.move =
