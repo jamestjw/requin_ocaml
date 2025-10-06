197b852445d6d5f5e13a258afa7328150c0c81f4
James Tan <james_tan97@outlook.com>
2025-10-05 23:44:00 -0400

    feat: implement pvs

--- lib/search.ml
+++ lib/search.ml
@@ -11,47 +11,73 @@ let initial_alpha = -T.value_mate - 1
 let initial_beta = T.value_mate + 1
 let generate_moves pos = M.generate_legal pos
 
-let rec alpha_beta pos curr_depth max_depth alpha beta is_white ply history ~may_prune ~tt
+let rec pvSearch
+          pos
+          curr_depth
+          max_depth
+          alpha
+          beta
+          is_white
+          ply
+          history
+          ~may_prune
+          ~tt
+          ~is_null_window
   =
+  let search move alpha beta ~is_null_window =
+    -1
+    * pvSearch
+        (P.do_move' pos move)
+        (curr_depth + 1)
+        max_depth
+        (-beta)
+        (-alpha)
+        (not is_white)
+        (ply + 1)
+        (move :: history)
+        ~may_prune:(not @@ P.is_capture pos move)
+        ~tt
+        ~is_null_window
+  in
   let remaining_depth = max_depth - curr_depth in
   let offset = if is_white then -1 else 1 in
   let eval_value = offset * Eval.evaluate pos () in
   let is_in_check = P.is_in_check pos in
-  let do_move (alpha, best_move) move =
+  let do_move (alpha, best_move, is_first_move) move =
     let score =
-      -1
-      * alpha_beta
-          (P.do_move' pos move)
-          (curr_depth + 1)
-          max_depth
-          (-beta)
-          (-alpha)
-          (not is_white)
-          (ply + 1)
-          (move :: history)
-          ~may_prune:(not @@ P.is_capture pos move)
-          ~tt
+      if is_first_move
+      then search move alpha beta ~is_null_window:false
+      else (
+        (* Search with null window, i.e. with [alpha , alpha + 1] *)
+        let score = search move alpha (alpha + 1) ~is_null_window:true in
+        if score > alpha && beta - alpha > 1
+        then
+          (* re-search with full window *)
+          search move alpha beta ~is_null_window:false
+        else score)
     in
     if score >= beta
     then (
       (* TODO: store killer moves *)
-      ignore
-      @@ TT.store
-           tt
-           ~key:pos.st.key
-           ~m:move
-           ~depth:remaining_depth
-           ~eval_value
-           ~value:score
-           ~bound:TT.BOUND_LOWER;
+      if not is_null_window
+      then
+        ignore
+        @@ TT.store
+             tt
+             ~key:pos.st.key
+             ~m:move
+             ~depth:remaining_depth
+             ~eval_value
+             ~value:score
+             ~bound:TT.BOUND_LOWER;
       Continue_or_stop.Stop (beta, best_move, true))
     else if score > alpha
-    then Continue_or_stop.Continue (score, Some move)
-    else Continue_or_stop.Continue (alpha, best_move)
+    then Continue_or_stop.Continue (score, Some move, false)
+    else Continue_or_stop.Continue (alpha, best_move, false)
   in
-  let do_move' alpha move =
-    match do_move (alpha, None) move with
-    | Continue_or_stop.Continue (score, m) -> score, m, false
+  let do_move' alpha move ~is_first_move =
+    match do_move (alpha, None, is_first_move) move with
+    | Continue_or_stop.Continue (score, m, _) -> score, m, false
     | Continue_or_stop.Stop (score, m, cut) -> score, m, cut
   in
   (* Try to get an early exit score from the TT entry, also evaluates the hash
@@ -69,13 +95,14 @@ let rec alpha_beta pos curr_depth max_depth alpha beta is_white ply history ~may
         | _, _ -> None
       in
       (match score, tt_entry.bound with
-       | Some _, _ -> score, alpha
+       | Some _, _ -> score, alpha, false
        | _, (TT.BOUND_EXACT | TT.BOUND_LOWER)
          when T.move_not_none tt_entry.move && P.legal pos tt_entry.move ->
-         let score, _, is_cut = do_move' alpha tt_entry.move in
-         if is_cut then Some score, alpha else None, score
-       | _, _ -> score, alpha)
-    | None -> None, alpha
+         (* Search hash move *)
+         let score, _, is_cut = do_move' alpha tt_entry.move ~is_first_move:true in
+         if is_cut then Some score, alpha, true else None, score, true
+       | _, _ -> score, alpha, false)
+    | None -> None, alpha, false
   in
   (* This takes into account the 50 move rule and threehold repetition *)
   if P.is_draw pos ply
@@ -104,8 +131,8 @@ let rec alpha_beta pos curr_depth max_depth alpha beta is_white ply history ~may
   else (
     let tt_entry = TT.probe tt pos.st.key in
     match process_tt_entry remaining_depth tt_entry alpha with
-    | Some score, _ -> score
-    | _, alpha ->
+    | Some score, _, _ -> score
+    | _, alpha, found_hash_move ->
       let legal_moves = M.generate_legal pos in
       if List.is_empty legal_moves
       then
@@ -133,25 +160,38 @@ let rec alpha_beta pos curr_depth max_depth alpha beta is_white ply history ~may
           |> List.map ~f:fst
         in
         let score, best_move, is_cut =
-          (* no cutoff if we finish *)
           List.fold_until
             sorted_moves
-            ~init:(alpha, None)
-            ~f:do_move
-            ~finish:(fun (a, b) -> a, b, false)
+            (* If we didn't a hash move, then we are processing the first move here *)
+            ~init:(alpha, None, not found_hash_move)
+            ~f:do_move (* No cutoff if we finish *)
+            ~finish:(fun (a, b, _) -> a, b, false)
         in
         if not is_cut
-        then
-          ignore
-          @@ TT.store
-               tt
-               ~key:pos.st.key
-               ~m:(Stdlib.Option.value best_move ~default:T.none_move)
-               ~depth:remaining_depth
-               ~eval_value
-               ~value:score
-               ~bound:
-                 (if Option.is_some best_move then TT.BOUND_EXACT else TT.BOUND_UPPER);
+        then (
+          match best_move, is_null_window with
+          | Some m, false ->
+            ignore
+            @@ TT.store
+                 tt
+                 ~key:pos.st.key
+                 ~m
+                 ~depth:remaining_depth
+                 ~eval_value
+                 ~value:score
+                 ~bound:TT.BOUND_EXACT
+          | None, _ ->
+            (* It's fine to set an upper bound even if we are doing a null move search *)
+            ignore
+            @@ TT.store
+                 tt
+                 ~key:pos.st.key
+                 ~m:T.none_move
+                 ~depth:remaining_depth
+                 ~eval_value
+                 ~value:score
+                 ~bound:TT.BOUND_UPPER
+          | _ -> ());
         score))
 ;;
 
@@ -164,7 +204,7 @@ let get_best_move (pos : P.t) max_depth ply : T.move =
            (* TODO: alpha beta from one move should be used to tighten subsequent searches *)
            Task.async pool (fun _ ->
              ( move
-             , -alpha_beta
+             , -pvSearch
                   (P.do_move' pos move)
                   0
                   curr_depth
@@ -174,7 +214,8 @@ let get_best_move (pos : P.t) max_depth ply : T.move =
                   (ply + 1)
                   [ move ]
                   ~may_prune:(not @@ P.is_capture pos move)
-                  ~tt )))
+                  ~tt
+                  ~is_null_window:false )))
        in
        let move_scores = List.map ~f:(Task.await pool) promises in
        (* TODO: add transposition table entry *)
